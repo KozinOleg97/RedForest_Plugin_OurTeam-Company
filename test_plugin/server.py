857fa46d942293c2f947dfc0f6a2348ab74e379c
Oleg Kosin <olegkosin@rambler.ru>
2019-01-06 22:33:36 +0300

    tornado

--- test_plugin/server.py
+++ test_plugin/server.py
@@ -1,22 +1,19 @@
-import asyncio
+import tornado.ioloop
+import tornado.web
 
 
-def run(self, host: str = "localhost", port: int = 8000, debug: bool = False):
-    """
-    start the http server
-    :param host: The listening host
-    :param port: The listening port
-    :param debug: whether it is in debug mod or not
-    """
-    if debug:
-        print("Nougat is listening on http://{}:{}\n".format(host, port))
-    self.debug = debug
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(self.start_server(host, port))
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        loop.close()
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
 
 
-run()
+def make_app():
+    return tornado.web.Application([
+        (r"/test", TestHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
